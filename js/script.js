f3890db5508eb8422b8b19957fe03b2dec0e9be6
Mohamed  Elsaeed <m01127673780@gmail.com>
2019-08-24 06:38:12 +0200

    before Add page Sunup

--- js/script.js
+++ js/script.js
@@ -116,34 +116,7 @@ $(".buttons button").click(function() {
 	
 	
 	/*start mag nfcal puoup  video*/  // plugin VideoPou
-	$('.video').magnificPopup({
-  type: 'iframe',
-  iframe: {
-    patterns: {
-      dailymotion: {
-       
-        index: 'dailymotion.com',
-        
-        id: function(url) {        
-            var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
-            if (m !== null) {
-                if(m[4] !== undefined) {
-                  
-                    return m[4];
-                }
-                return m[2];
-            }
-            return null;
-        },
-        
-        src: 'https://www.youtube.com/watch?v=0zZtzeXycW4/embed/video/%id%'
-        
-      }
-    }
-  }
-  
-  
-});
+ 
  /*End mag nfcal puoup  video*/
 	
 	
@@ -801,8 +774,72 @@ $(".price_value").html('60 Mins');
       $('.nav').animate({top: '-50px'}, 1000);
       $('.topbar').addClass("isUp");
     }  
+
+
+    
   });
 
 
+/*
+  Inspired by: "Login Page & Homepage"
+  By: Neo
+  Link: https://dribbble.com/shots/4485321-Login-Page-Homepage
+*/
+
+let usernameInput = document.querySelector('.username');
+let passwordInput = document.querySelector('.password');
+let showPasswordButton = document.querySelector('.password-button');
+let face = document.querySelector('.face');
+
+passwordInput.addEventListener('focus', event => {
+  document.querySelectorAll('.hand').forEach(hand => {
+    hand.classList.add('hide');
+  });
+  document.querySelector('.tongue').classList.remove('breath');
+});
+
+passwordInput.addEventListener('blur', event => {
+  document.querySelectorAll('.hand').forEach(hand => {
+    hand.classList.remove('hide');
+    hand.classList.remove('peek');
+  });
+  document.querySelector('.tongue').classList.add('breath');
+});
+
+usernameInput.addEventListener('focus', event => {
+  let length = Math.min(usernameInput.value.length - 16, 19);
+  document.querySelectorAll('.hand').forEach(hand => {
+    hand.classList.remove('hide');
+    hand.classList.remove('peek');
+  });
+
+  face.style.setProperty('--rotate-head', `${-length}deg`);
+});
+
+usernameInput.addEventListener('blur', event => {
+  face.style.setProperty('--rotate-head', '0deg');
+});
+
+usernameInput.addEventListener('input', _.throttle(event => {
+  let length = Math.min(event.target.value.length - 16, 19);
+
+  face.style.setProperty('--rotate-head', `${-length}deg`);
+}, 100));
+
+showPasswordButton.addEventListener('click', event => {
+  if (passwordInput.type === 'text') {
+    passwordInput.type = 'password';
+    document.querySelectorAll('.hand').forEach(hand => {
+      hand.classList.remove('peek');
+      hand.classList.add('hide');
+    });
+  } else {
+    passwordInput.type = 'text';
+    document.querySelectorAll('.hand').forEach(hand => {
+      hand.classList.remove('hide');
+      hand.classList.add('peek');
+    });
+  }
+});
 
 
